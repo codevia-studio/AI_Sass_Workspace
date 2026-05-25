0bcd08db629852a84372688ddaf308ffa24e4a94
ali sadeghi <alisadghi228@gmail.com>
2026-05-25 12:38:08 +0330

    init

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+import { ThemeToggle } from "@/components/ui/theme-togle";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <ThemeToggle />
+    </div>
+  );
+}
